adae760262ea55e2db95d39d942a284ec463a1d3
Vikash-sharmaa <vikashs2000@gmail.com>
2021-09-29 00:10:49 +0530

    Update Stock_Span_Problem.cpp

--- Stack/Stock_Span_Problem.cpp
+++ Stack/Stock_Span_Problem.cpp
@@ -1,16 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//store index of greater element in vector
+//store pair of greater element and its index in  stack
+
 vector<int> stock_span(int *a)
 {
-  vector<pair<int,int>>res;
-  vector<int>result;
-  stack<pair<int,int>>s;
+  vector<int> res;
+  stack<pair<int,int>> s;
   for(int i=0;i<7;i++)
   {
-    if(s.empty()) res.push_back({-1,-1});
+    if(s.empty()) res.push_back(-1);
+    else if(s.top().first > a[i]) res.push_back(s.top().second);
+    else
+    {
+      while(s.size()>0 && s.top().first <= a[i])
+      {
+        s.pop();
+      }
+      if(s.size()==0) res.push_back(-1);
+      else res.push_back(s.top().second);
+    }
+    s.push({a[i],i});
   }
-  return result;
+  for(int i=0;i<7;i++)
+  {
+    res[i]=i-res[i];
+  }
+  return res;
 }
 
 int main()
